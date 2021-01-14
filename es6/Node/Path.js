63f4993160d84abb741517efb76f1162e4b5182e
vikky <vikkyhp0007@gmail.com>
2021-01-14 23:54:45 +0530

    first

---
+++ es6/Node/Path.js
@@ -0,0 +1,24 @@
+// 🏷️ path module 🔥
+
+const path = require("path");
+console.log(__dirname);
+const fileLocation = path.join(__dirname, "fs.js");
+console.log(fileLocation);
+
+// return the basename of whole path
+// const filename = path.basename(fileLocation);
+// console.log(filename);
+
+// // for extension
+// console.log(path.extname(filename));
+
+// check if path is absolute path
+// console.log(path.isAbsolute(fileLocation)); //true
+// console.log(path.isAbsolute(filename)); //false
+// console.log(path.isAbsolute("/")); //true
+// console.log(path.isAbsolute(".")); //false
+// console.log(path.isAbsolute(" ")); //false
+
+// 🟥 parse it return an object
+console.log(path.parse(fileLocation));
+console.log(path.normalize(fileLocation));
