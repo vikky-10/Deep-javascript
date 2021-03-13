faea6d4585da8f999f700c221aebdf66f7443a34
vikky <vikkyhp0007@gmail.com>
2021-03-13 23:03:31 +0530

    first

--- Deep_java_kyle/fundamental_objects.js
+++ Deep_java_kyle/fundamental_objects.js
@@ -22,3 +22,38 @@ console.log(" 🥇 javascript objects");
 // console.log(text);
 
 //The delete keyword deletes both the value of the property and the property itself
+
+// 🏷️ javascript -Auto-Boxing
+// when necessary , primitive type are wrapped by identically-named objects, and then back to their primitive types again
+
+// var b = new Boolean(false);
+// console.log(typeof b); //object
+// console.log(typeof true); //boolean
+
+// primitive types are immutable
+// Direct use of the boxed types(i.e new String('wrong);)
+// is almost always a mistake
+// var declaration are not blocked scoped
+// var declation are hoisted: it's as if they're all declared at the top of the global or function scope in which they're define
+// function foo() {
+//   var x;
+
+//   console.log(x);
+//   x = 15;
+// }
+// foo(); //undefine
+// let declations are not hoisted
+//  polyfills & transpilers check for , and enforce this at build time
+// function foo() {
+//   console.log(x);
+//   let x = 10;
+// }
+// foo(); //referenceerror x is not define
+// const declation are not hoisted
+// must be initialized at the time of declation
+// reassignment is not allowed
+// Constant variable doesnot mean "immutable valuew"
+// console.log("30" - 7);//23
+// console.log("30" + 7);//307
+
+// key difference is:javascript uses "dynamic types"(resolved at runtime), TypeScript uses "static types"(set durring development )
