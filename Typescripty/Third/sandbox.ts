aaaf09edbdf7c392fccb597346d2aebedb47cef3
vikky <vikkyhp0007@gmail.com>
2021-06-04 01:21:14 +0530

    add

--- Typescripty/Third/sandbox.ts
+++ Typescripty/Third/sandbox.ts
@@ -8,10 +8,49 @@
 //   console.log(input);
 // });
 
-let character = "vikky";
-let age = 30;
-let isBoolean = true;
+// let character = "vikky";
+// let age = 30;
+// let isBoolean = true;
 
-let mixed = ["man", 4, "singh", 78];
-mixed.push(45);
-mixed.push("amit");
+// let mixed = ["man", 4, "singh", 78];
+// mixed.push(45);
+// mixed.push("amit");
+
+// let object={
+//     name:'vikkt',
+//     class:'10',
+//     rollno:'22'
+// }
+// object.name="singh";
+// object.password="vtt1475"; //it gives us error because password property is not present in object
+
+// this is also not accepted beacuse it to matches with total no of property rollno is missing
+// object={
+//     name:'yosi',
+//     class:'8',
+// }
+
+// explicity type
+// let character: string;
+// let age: number;
+
+// let arr: string[] = [];
+
+// union
+// let arr1: (string | number)[] = [];
+// arr1.push("vikky");
+// arr1.push(45);
+
+// let uid: number | string | boolean;
+// uid = "vikky";
+// uid = 1452;
+// uid = true;
+
+// let myobj: object;
+// myobj = {
+//   name: "vikky",
+//   rollno: 47,
+// };
+
+// this not give an error beacuse in js arr is an object type
+// myobj=["vikky",45];
