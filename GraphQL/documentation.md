079b5fae5a1ef9cd569af197a8e4e31e33e89352
Vikky Singh <vikkyhp0007@gmail.com>
2021-12-09 15:30:16 +0530

    graphql

--- GraphQL/documentation.md
+++ GraphQL/documentation.md
@@ -1,13 +1,25 @@
 🔥 GraphQL
 
 <!-- --------- -->
+graphQL!==database  !important
+ 
+
+it is use to cummunicate between client and server
 
 🔴 it is a powerfull query language for api.
 🔴 it allows for a more flexible and efficient approach than rest.
 🔴 While typical REST APIs require loading from multiple URLs, GraphQL APIs get all the data your app needs in a single request.
 🔴 Apps using GraphQL can be quick even on slow mobile network connections.
 🔴 GraphQL is self documented
-🔴 
+🔴  get request is know as Query ant other like put, delete, post are know as mutation
+
+in this i created application 
+i you graphql server for backend 
+React as frontend and node as backend
 
 
-
+🎯🎯🎯most important thisng is that ke we are requestion to single end point "/graphql"  
+for example:    
+/user-------------------  /grphql
+/follower---------------  /grphql
+/posts------------------/graphql
---
+++ GraphQL/graphql2/project1/index.js
@@ -0,0 +1,53 @@
+//  createing a server
+
+const { ApolloServer, gql } = require("apollo-server");
+
+// define a typedefs or schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+    numberofappe: Int
+    isCool: Boolean
+
+    # what does it means  it means it accept string or null
+    # if i not pass any value by default it is null
+    # if you want not pass null add ! at the end
+    checknullable: String
+
+    # array of strings
+    # newArr: [String]
+    # it could be array of string or null but inside arr it not entertain null value inside array
+    # newArr: [String!]
+
+    # if you want it also not return null so add !
+    newArr: [String!]!
+  }
+`;
+// resolver
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Welcome vikky hope you doing good!!";
+    },
+    numberofappe: () => {
+      return 5;
+    },
+    isCool: () => {
+      return false;
+    },
+    checknullable: () => {
+      return;
+    },
+    newArr: () => {
+      return ["Hello", "my", "Friend"];
+      // return ["Hello", null, "Friend"];
+      // return null;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log("server is ready at " + url);
+});
